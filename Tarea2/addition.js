280d829bc3f3fdc84dd3afd57c33301be9b0baa5
Ignacio Cl <juanigcl2532@gmail.com>
2021-06-21 20:05:32 -0600

    Agrego cambios en la tarea

--- Tarea2/addition.js
+++ Tarea2/addition.js
@@ -1,26 +1,22 @@
-exports.fruits = function(fruit){
-    let array = ['Manzana', 'Pera', 'Uva', 'Sandía', 'Banano', 'Melocotón', 'Piña', 'Maracuyá', 'Mandarina', 'Mango'];
-    for(let i = 0; i < array.length; i++)
-    if(array[i] === fruit){
-        console.log('Si la tenemos disponible')
-        break
-    }else{
-        console.log('No la tenemos disponible')
-        break
-    }
+/* let status = false;  Porque aqui tambien funciona, se sobreescribe ? */
+function fruitExist(){
+    console.log('Si tenemos esa fruta')
 }
 
-/*
+function fruitDoesntExist(){
+    console.log('No tenemos esa fruta')
+}
 exports.fruits = function(fruit){
+    let status = false;
     let array = ['Manzana', 'Pera', 'Uva', 'Sandía', 'Banano', 'Melocotón', 'Piña', 'Maracuyá', 'Mandarina', 'Mango'];
-    for(let i = 0; i < array.length; i++)
-    if(array[i] === fruit.toString()){
-        console.log('Si la tenemos disponible')
-        break
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === fruit){
+            status = true;
+        }
+    }
+    if(status === true){
+        fruitExist()
     }else{
-        console.log('No la tenemos disponible')
-        break
+        fruitDoesntExist()
     }
 }
-
-*/
